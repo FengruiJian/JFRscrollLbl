5fb2c60fd8781df0b58c612d7ce73fa9d0161a2e
mac <mac@macdemacbook-air.local>
2016-11-24 13:58:58 +0800

    Initial Commit

---
+++ JFRscrollLbl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JFRscrollLbl
+//
+//  Created by mac on 16/11/24.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JFRscrollLbl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JFRscrollLbl
+//
+//  Created by mac on 16/11/24.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
